2d92b3b8eda6f5b65a8b3fbf0929fd7bb4f85d31
Claudiu <claudiu.ciuca95@gmail.com>
2026-01-02 20:19:15 +0200

    Added task 1 and 2

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 let students = [];
 
 function getUserInfo() {
-    let nrStudents = prompt(`How many students do you want to mention?`)
+  let nrStudents = prompt(`How many students do you want to mention?`);
+  nrStudents = Number(nrStudents);
+
+  for (let i = 0; i < nrStudents; i++) {
+    const studentName = prompt(`Please enter the name for student ${i + 1}:`);
+    let studentAge = prompt(`How old is ${studentName}?`);
+    const studentAttendance = confirm(`Is ${studentName} present?`);
+
+    students.push({
+      name: studentName,
+      age: studentAge,
+      attendance: studentAttendance,
+    });
+  }
+  return students;
 }
+console.log(getUserInfo());
